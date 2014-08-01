fee98dd990fcc48788d1b1a289756486b62b8c87
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-08-01 10:32:51 +0200

    Example process manager in test.

---
+++ src/test/scala/es/infrastructure/akka/CounterProcessManager.scala
@@ -0,0 +1,35 @@
+package es.infrastructure.akka
+
+import java.util.UUID
+
+import es.api.ProcessManager.SubscribeToAggregateType
+import es.api.{EventData, ProcessManagerType}
+import es.infrastructure.akka.counter.Incremented
+
+import scala.util.Try
+
+class CounterProcessManager extends ProcessManagerType {
+  def name = "Counter"
+  type Id = UUID
+
+  type Command = PairThem
+  case class Manager(id: UUID, round: Int) extends BaseManager {
+    def handle = {
+      case EventData(_, _, _, Incremented(i)) if i % 2 == 0 =>
+        val cmd = PairThem(i % 2, round)
+        (cmd :: Nil, Nil, Right(copy(round = round + 1)))
+    }
+  }
+
+  def triggeredBy = SubscribeToAggregateType(counter) :: Nil
+  def initiate = {
+    //one process manager per counter
+    case EventData(counter, id, _, _) => id.asInstanceOf[UUID]
+  }
+
+  def serializeId(id: Id) = id.toString
+  def parseId(serialized: String) = Try(UUID.fromString(serialized)).toOption
+  def seed(id: Id) = Manager(id, 1)
+}
+
+case class PairThem(numberOfPairs: Int, round: Int)
