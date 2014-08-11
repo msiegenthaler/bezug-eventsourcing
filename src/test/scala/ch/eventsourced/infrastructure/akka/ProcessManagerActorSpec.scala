66db141be2792080ffc6983477c248c3e799863f
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-08-11 12:30:03 +0200

    ProcessManagerActor spec.

--- src/test/scala/ch/eventsourced/infrastructure/akka/ProcessManagerActorSpec.scala
+++ src/test/scala/ch/eventsourced/infrastructure/akka/ProcessManagerActorSpec.scala
@@ -1,23 +1,95 @@
 package ch.eventsourced.infrastructure.akka
 
+import akka.testkit.TestProbe
+import ch.eventsourced.api.AggregateKey
+import ch.eventsourced.infrastructure.akka.AggregateManager.{AggregateEvent, Execute, UnsubscribeFromAggregate, SubscribeToAggregate}
+import ch.eventsourced.infrastructure.akka.Order.{StartOrder, OrderPlaced}
+
 class ProcessManagerActorSpec extends AbstractSpec {
 
-  "processManager actor" must {
-    "start the process on initiation event" in ???
+  val testProbe = TestProbe()
+  val pma = new ProcessManagerActor("test", OrderProcess, testProbe.ref)(system, 10)
 
-    "send the start event to the process manager" in ???
-
-    "auto-setup a subscription to the source of the initial event" in ???
-
-    "support subscribing to another aggregates" in ???
-
-    "support unsubscribing from an aggregate" in ???
-
-    "newly set up subscription must be retroactive" in ???
-
-    "initial subscription must not be retroactive" in ???
-
-    "next event from the initial subscription must be received" in ???
+  implicit class RichCommandProbe(t: TestProbe) {
+    def expectSubscribe() = {
+      t.expectMsgPF(hint = s"subscribe starting at 0") {
+        case SubscribeToAggregate(sid, to, _, 0, ack) => (sid, ack, to)
+      }
+    }
+    def expectSubscribe(to: AggregateKey, from: Long) = {
+      t.expectMsgPF(hint = s"subscribe to $to starting at $from") {
+        case SubscribeToAggregate(sid, `to`, subscriber, `from`, ack) => (sid, subscriber, ack)
+      }
+    }
+    def expectUnsubscribe(sid: String, from: AggregateKey) = {
+      t.expectMsgPF(hint = s"unsubscribe $sid from $from") {
+        case UnsubscribeFromAggregate(`sid`, `from`, ack) => ack
+      }
+    }
+    def expectCommand[A](pf: PartialFunction[OrderProcess.Command, A]) = {
+      t.expectMsgPF(hint = s"command") {
+        case Execute(cmd, ok, fail) if pf.isDefinedAt(cmd) => (ok, fail, pf(cmd))
+      }
+    }
   }
 
+  "processManager actor" must {
+    "start a process manager instance on initiation message" in {
+      val orderId = StartOrder().order
+      val op = OrderPlaced(Nil, 100, "test-ref")
+      val ope = Order.EventData(orderId, 1, op)
+      val id = pma.processManagerType.initiate(ope)
+
+      pma.ref ! pma.ProcessInitationMessage(id, ope, "ack1")
+      expectMsg("ack1")
+
+      val (orderSubscription, subscriber, ackSubO) = testProbe.expectSubscribe(ope.aggregateKey, ope.sequence)
+      testProbe.reply(ackSubO)
+      system.actorSelection(subscriber) ! AggregateEvent(orderSubscription, ope, "ack2")
+      expectMsg("ack2")
+
+
+      //Request payment
+      val (ackRequest, _, paymentKey2) = testProbe.expectCommand {
+        case Payment.RequestPayment(100, "test-ref", id) => id
+      }
+      testProbe.reply(ackRequest)
+      //Subscribe to payment
+      val (_, ackSubP, paymentKey) = testProbe.expectSubscribe()
+      testProbe.reply(ackSubP)
+      //Unsubscribe from order
+      val ackUnsO = testProbe.expectUnsubscribe(orderSubscription, ope.aggregateKey)
+      testProbe.reply(ackUnsO)
+      assert(paymentKey.aggregateType == Payment)
+      assert(paymentKey.id === paymentKey2)
+    }
+
+    "subscribe with a absolute actor path (/user/)" in {
+      val orderId = StartOrder().order
+      val op = OrderPlaced(Nil, 100, "test-ref")
+      val ope = Order.EventData(orderId, 1, op)
+      val id = pma.processManagerType.initiate(ope)
+
+      pma.ref ! pma.ProcessInitationMessage(id, ope, "ack1")
+      expectMsg("ack1")
+
+      val (orderSubscription, subscriber, ackSubO) = testProbe.expectSubscribe(ope.aggregateKey, ope.sequence)
+      testProbe.reply(ackSubO)
+      assert(subscriber.elements.head === "user")
+    }
+
+    "subscribe to path of the sharding root" in {
+      val orderId = StartOrder().order
+      val op = OrderPlaced(Nil, 100, "test-ref")
+      val ope = Order.EventData(orderId, 1, op)
+      val id = pma.processManagerType.initiate(ope)
+
+      pma.ref ! pma.ProcessInitationMessage(id, ope, "ack1")
+      expectMsg("ack1")
+
+      val (orderSubscription, subscriber, ackSubO) = testProbe.expectSubscribe(ope.aggregateKey, ope.sequence)
+      testProbe.reply(ackSubO)
+      assert(subscriber === pma.ref.path)
+    }
+  }
 }
